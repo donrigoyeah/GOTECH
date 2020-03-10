f0bfb8fafa45ad9c99b0e2072815d7bf4e2a6764
donrigoyeah <donrigo2@googlemail.com>
2020-03-10 17:21:38 +0100

    changes to reference of js file

--- master.js
+++ master.js
@@ -2,7 +2,7 @@ function filterSelection(c) {
 
     var x = c;
 
-    y = document.getElementsByClassName(x);
+    y = document.getElementsByClassName("x");
 
     console.log(x);
     console.log(y);
